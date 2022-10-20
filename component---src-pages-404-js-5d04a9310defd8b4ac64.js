9cc889b64ca752aa21b0a3b982387abb0224beba
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-10-20 13:22:02 +0100

    Updates

---
+++ component---src-pages-404-js-5d04a9310defd8b4ac64.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[883],{429:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(7294);function a(){return n.createElement(n.Fragment,null,n.createElement("div",null,"Error Page"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5d04a9310defd8b4ac64.js.map
